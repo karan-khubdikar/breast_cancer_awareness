6cd2c7482e62587accb8a5dbee23eac34739f3ed
Karan Khubdikar <karan647@student.ubc.ca>
2024-04-16 02:19:18 -0700

    Updated charts and tables

--- src/app.R
+++ src/app.R
@@ -1,57 +1,103 @@
-# If you want the same port each time and enable hot reloading
-options(shiny.port = 8050, shiny.autoreload = TRUE)
-
-library(shiny)
-library(ggplot2)
-library(dplyr)
-library(tidyr)
-library(readr)
-
-# No explicit initialization in Shiny
-
-# Data loading
-age_data <- read.csv("../data/filtered/age_data.csv") 
-type_data <- read_csv("../data/filtered/canc_type_data.csv")
-
-# Layout
-ui <- fluidPage(
-  h1("Breast Cancer Awareness"),
-  p("This Dashboard provides a brief summary about the number of new breast cancer cases in Canada over the past 10 years"),
-  p(""),
-  plotOutput("bar_plot", width = "500px"),  # Change 'plot' to 'bar_plot'
-  # plotOutput("pie_chart"),
-  selectInput(
-    'x_col',
-    'SELECT PROVINCE',
-    choices = age_data$GEO,
-    selected = 'Alberta'
-  )
-)
-
-# Server side callbacks/reactivity
-server <- function(input, output, session) {
-  output$bar_plot <- renderPlot({  # Change 'plot' to 'bar_plot'
-    filtered_data <- age_data %>%
-      filter(GEO == input$x_col) %>%
-      mutate(VALUE = round(VALUE,0))
-    
-    ggplot(filtered_data, aes(x = Age_group, y = VALUE)) +
-      geom_bar(stat = "identity", fill = 'darkblue')+
-      geom_text(aes(label = VALUE), vjust = -0.5, color = "black", size = 3.5)+
-      labs(x = "Age Group",
-           y = "Avg no. of new Breast Cancer cases",
-           title = "Average no. of cases across groups")
-  })
-  output$pie_chart <- renderPlot({
-    filtered_data2 <- type_data %>%
-      filter(GEO == input$x_col) %>%
-      mutate(VALUE = round(Percentage, 2))
-    
-    # Create pie chart
-    pie(filtered_data2$Percentage, labels = filtered_data2$Cancer_type, main = "Distribution of Cancer Types")
-  })
+  # If you want the same port each time and enable hot reloading
+  options(shiny.port = 8050, shiny.autoreload = TRUE)
   
-}
+  library(shiny)
+  library(ggplot2)
+  library(dplyr)
+  library(tidyr)
+  library(readr)
+  
+  # No explicit initialization in Shiny
+  
+  # Data loading
+  age_data <- read.csv("../data/filtered/age_data.csv") 
+  type_data <- read_csv("../data/filtered/canc_type_data.csv")
+  year_data <- read_csv("../data/filtered/year_data.csv")
+  
+  
+  # Layout
+  ui <- fluidPage(
+    titlePanel("Breast Cancer Awareness"),
+    p("This Dashboard provides a brief summary about the number of new breast cancer cases in Canada over 10 years(2012-2021)"),
+    p(""),
+    tags$style(type = "text/css", 
+               ".column-panel { background-color: #f0f0f0; padding: 10px; }"),
+    fluidRow(
+    column(
+      width = 2,
+      class = 'column-panel',
+      selectInput(
+        'x_col_year',
+        'SELECT YEAR (table)',
+        choices = year_data$DATE,
+        selected = year_data$DATE[1]
+      ),
+      selectInput(
+        'x_col',
+        'SELECT PROVINCE (bar)',
+        choices = age_data$GEO,
+        selected = age_data$GEO[1]
+      ),
+      selectInput(
+        'x_col_pie',
+        'SELECT PROVINCE (pie)',
+        choices = type_data$GEO,
+        selected = type_data$GEO[1]
+      ),
+    ),
+    column(
+      width = 9,
+      tableOutput('table'),
+      plotOutput("pie_chart", width = "100%")
+    )
+  ),
+    mainPanel(
+      plotOutput("bar_plot", width = "600px"),
+    )
+  )
 
-# Run the app/dashboard
-shinyApp(ui, server)
+  
+  # Server side callbacks/reactivity
+  server <- function(input, output, session) {
+    output$bar_plot <- renderPlot({  # Change 'plot' to 'bar_plot'
+      filtered_data <- age_data %>%
+        filter(GEO == input$x_col) %>%
+        mutate(VALUE = round(VALUE,0))
+      
+      ggplot(filtered_data, aes(x = Age_group, y = VALUE)) +
+        geom_bar(stat = "identity", fill = 'darkblue')+
+        geom_text(aes(label = VALUE), vjust = -0.5, color = "black", size = 3.5)+
+        labs(x = "Age Group",
+             y = "Avg no. of new Breast Cancer cases",
+             title = "Average no. of cases across groups")
+    })
+    output$pie_chart <- renderPlot({
+      filtered_data2 <- type_data %>%
+        filter(GEO == input$x_col_pie) %>%
+        mutate(VALUE = round(Percentage, 2))
+      
+      # Create pie chart
+      ggplot(filtered_data2, aes(x = "", y = VALUE, fill = Cancer_type)) +
+        geom_bar(stat = "identity", width = 1) +
+        geom_text(aes(label = paste0(round(VALUE/sum(VALUE) * 100), "%")), 
+                  position = position_stack(vjust = 0.5), 
+                  color = "white", size = 5) +  
+        coord_polar(theta = "y") +
+        theme_void() +
+        theme(legend.position = "right")+
+        labs(title = "Distribution of top 5 Cancers", fill = "Cancer Types")
+    })
+    output$table <- renderTable({
+      table_data <- year_data %>%
+        filter(DATE == input$x_col_year) %>%
+        dplyr::select(GEO, VALUE) %>%
+        mutate(VALUE = round(VALUE,0)) %>%
+        arrange(desc(VALUE)) %>%
+        head(5)
+      return(table_data)
+    })
+    
+  }
+  
+  # Run the app/dashboard
+  shinyApp(ui, server)
