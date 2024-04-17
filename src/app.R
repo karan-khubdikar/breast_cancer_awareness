0d5cd4ca920e85d81ecc0660b345bfb085d276b1
Karan Khubdikar <karan647@student.ubc.ca>
2024-04-16 23:00:39 -0700

    Updated layout

--- src/app.R
+++ src/app.R
@@ -17,66 +17,100 @@
   
   # Layout
   ui <- fluidPage(
-    titlePanel("Breast Cancer Awareness"),
+    tags$head(
+      tags$style(
+        HTML(
+          ".dashboard-header {
+          background-color: #ffc0cb; /* Change the color code to your desired color */
+          color: black; /* Change the text color if needed */
+          padding: 10px; /* Adjust the padding as needed */
+        }
+        .chart-container {
+          border: 1px solid #ccc; /* Add grey border */
+          background-color: #f0f0f0; /* Add grey background */
+          padding: 10px; /* Adjust the padding as needed */
+          margin-bottom: 20px; /* Add margin bottom for spacing */
+        }"  
+        )
+      )
+    ),
+    div(class = "dashboard-header",
+        titlePanel("Breast Cancer Awareness"),
     p("This Dashboard provides a brief summary about the number of new breast cancer cases in Canada over 10 years(2012-2021)"),
-    p(""),
+    ),
+        p(""),
     tags$style(type = "text/css", 
                ".column-panel { background-color: #f0f0f0; padding: 10px; }",
-               ".main-panel { border: 1px solid #ccc; padding: 10px; margin-bottom: 20px; }",
-               ".table-container { border: 1px solid #ccc; padding: 10px; }"),
+               ".main-panel { border: 1px solid #ccc; padding: 5px; margin-bottom: 10px; }",
+               ".table-container { border: 1px solid #ccc; padding: 5px; }"),
     tags$style(type = "text/css", 
                ".column-panel { background-color: #f0f0f0; padding: 10px; }"),
     sidebarPanel(
            selectInput(
         'x_col_year',
-        'SELECT YEAR (table)',
+        'SELECT YEAR (for table)',
         choices = year_data$DATE,
-        selected = 2015
-      ),
-      selectInput(
-        'x_col',
-        'SELECT PROVINCE (bar)',
-        choices = age_data$GEO,
-        selected = age_data$GEO[1]
+        selected = 2012
       ),
       selectInput(
         'x_col_pie',
-        'SELECT PROVINCE (pie)',
+        'SELECT PROVINCE (for pie chart)',
         choices = type_data$GEO,
-        selected = type_data$GEO[1]
+        selected = 'Alberta'
+      ),
+      selectInput(
+        'x_col',
+        'SELECT PROVINCE (for bar chart)',
+        choices = age_data$GEO,
+        selected = 'Alberta'
       )
   ),
-    mainPanel(
-      fluidRow(
-        column(
-          width = 4,
-        h5("Top 5 Provinces (new cases of Breast Cancer)"),
-        tableOutput('table'),),
-        column(
-          width = 8,
-        h5("Distribution of top 5 Cancers"),
-        plotOutput("pie_chart",height = "200px"),)),
-      h5("Average no. of cases across groups"),
-      plotOutput("bar_plot",height = "400px"),
+  mainPanel(
+    fluidRow(
+      column(
+        width = 5,
+        div(class = "chart-container",
+            h4("Top 5 Provinces (new cases of Breast Cancer)"),
+            tableOutput('table')
+        )
+      ),
+      column(
+        width = 7,
+        div(class = "chart-container",
+            h4("Distribution of Top 5 Cancers"),
+            plotOutput("pie_chart", height = "220px")
+        )
+      )
+    ),
+    div(class = "chart-container",
+        h4("Average no. of cases across Age groups"),
+        plotOutput("bar_plot", height = "400px")
     )
+  )
   )
 
   
   # Server side callbacks/reactivity
   server <- function(input, output, session) {
+    observe({
+      updateSelectInput(session, "x_col_year", selected = year_data$DATE[1])
+      updateSelectInput(session, "x_col", selected = age_data$GEO[1])
+      updateSelectInput(session, "x_col_pie", selected = type_data$GEO[1])
+    })
     output$bar_plot <- renderPlot({  # Change 'plot' to 'bar_plot'
       filtered_data <- age_data %>%
         filter(GEO == input$x_col) %>%
         mutate(VALUE = round(VALUE,0))
       
       ggplot(filtered_data, aes(x = Age_group, y = VALUE)) +
-        geom_bar(stat = "identity", fill = 'purple')+
-        geom_text(aes(label = VALUE), vjust = -0.5, color = "black", size = 3.5)+
+        geom_bar(stat = "identity", fill = 'pink')+
+        geom_text(aes(label = VALUE), vjust = -0.5, color = "black", size = 5)+
         labs(x = "Age Group",
              y = "Avg no. of new Breast Cancer cases")+
         theme_minimal()+
         theme(panel.grid.major = element_blank(),
-              panel.grid.minor = element_blank())
+              panel.grid.minor = element_blank(),
+              axis.text = element_text(color = "black", size = 12))
     })
     output$pie_chart <- renderPlot({
       filtered_data2 <- type_data %>%
@@ -97,10 +131,13 @@
     output$table <- renderTable({
       table_data <- year_data %>%
         filter(DATE == input$x_col_year) %>%
-        dplyr::select(GEO, VALUE) %>%
-        mutate(VALUE = round(VALUE,0)) %>%
-        arrange(desc(VALUE)) %>%
-        head(5)
+        dplyr::select(Province = GEO, Value = VALUE) %>%
+        mutate(Value = round(Value, 0)) %>%
+        arrange(desc(Value)) %>%
+        head(5) %>%
+        mutate(rank = as.integer(rank(-Value)),
+               Value = as.integer(Value)) %>%
+        select(rank, Province, Value) 
       return(table_data)
     })
     
